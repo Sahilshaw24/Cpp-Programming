4e2547489a7a5bfb14044871ca0b94db2bc22a45
Sahil Shaw <shaws2857@gmail.com>
2025-09-14 12:08:49 +0530

    first commit

--- Strings/basic.cpp
+++ Strings/basic.cpp
@@ -1,4 +1,32 @@
 #include<iostream>
 using namespace std;
 
-cout << "hello" << endl;
+int main(){
+     // // Using Character with size
+     // char arr[10];
+     // cin >> arr;
+     // cout << arr;
+
+     // // Using String
+     // string s;
+     // // cin >> s; // Single word store
+     // getline(cin,s); // Multiple senstense
+     // cout << s;
+
+     // // Escape Character
+     // string s = "Sahil is a \"good\" boy";
+     // cout << s;
+
+     // Reverse String
+     string str = "123456";
+     int start = 0;
+     int end = str.size() - 1;
+     while(start < end){
+          int temp = str[start];
+          str[start] = str[end];
+          str[end] = temp;
+          start++;
+          end--;
+     }
+     cout << str;
+}
